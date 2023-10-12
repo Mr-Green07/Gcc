72c7636a02eb520719d98768e1013f3c0cda5b40
sigma web <yuvrajkumar5070@gmail.com>
2023-10-12 15:16:00 +0530

    array

--- nested loop/one.c
+++ nested loop/one.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
-int main() {
-  int n, i;
-  printf("Aadityavarrdhan Jay Singh \n");
-  printf("UID-23BSC13529 \n");
-  printf("Enter an integer: ");
-  scanf("%d", &n);
-
-  for (i=1; i<=10; i++){
-   printf("%d\n",n*i);
-  }
- return 0;
+#include<stdio.h>
+int main(){
+    int i,j;
+    for(i=0;i<5;i++){
+        for(j=0;j<5;j++){
+            printf("%d %d\n",i,j);
+        }
+    }
+    return 0;
 }
